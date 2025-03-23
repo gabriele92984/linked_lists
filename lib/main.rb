a6c88f2ed7454bd0dce98154d9c5cf1bd672ba40
gabriele92984 <60701037+gabriele92984@users.noreply.github.com>
2025-03-23 14:46:21 +0100

    Add #append method

--- lib/main.rb
+++ lib/main.rb
@@ -8,9 +8,44 @@ class Node
 end
 
 class LinkedList
-  attr_accessor :head
+  attr_accessor :head, :tail
 
   def initialize
     @head = nil
+    @tail = nil
+  end
+
+  def append(data)
+    node = Node.new(data)
+    if @head.nil?
+      @head = node
+      @tail = node
+    else
+      @tail.next = node
+      @tail = node
+    end
+  end
+
+  def to_s
+    string = ""
+    current_node = @head
+    while current_node
+      string += "( #{current_node.data} ) -> "
+      current_node = current_node.next
+    end
+
+    string += "nil"
+    string
   end
 end
+
+list = LinkedList.new
+
+list.append('dog')
+list.append('cat')
+list.append('parrot')
+list.append('hamster')
+list.append('snake')
+list.append('turtle')
+
+puts list
